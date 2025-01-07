9bf52bfec17886be7dd33b0d59888ba38c1da675
volkovMikhail <volkov.work1@gmail.com>
2025-01-07 08:52:13 +0300

    add tag list to /blog, dynamic year

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,12 +3,13 @@ module.exports = {
 	url: "https://betaheads.net",
 	language: "en",
 	description: "Betaheads minecraft server on version beta 1.7.3",
-	keywords: "minecraft, beta, 1.7.3, server, old, nostalgia, сервер, майнкрафт, бета",
+	keywords:
+		"minecraft, beta, 1.7.3, server, old, nostalgia, сервер, майнкрафт, бета",
 	author: {
 		name: "Betaheads",
 		url: "",
 		email: "",
 		discord: "https://discordapp.com/users/360492946455265281",
 	},
-	year: "2024"
+	year: new Date().getFullYear().toString(),
 };
